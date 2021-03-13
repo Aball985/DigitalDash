2bc6d167e50e11389f066d474651c5d8f305bf21
Aball985 <arball44@gmail.com>
2021-03-13 12:10:12 -0500

    test

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -7,7 +7,11 @@ const Dashboard = () => {
         <h1>Left Side</h1>
       </div>
       <div className="right__Sidebar">
-        <h1>Right Side</h1>.
+        <h1>Right Side</h1>
+        <div className="top__Navbar"></div>
+        <div className="middle__Content">
+          <h1>This is content</h1>
+        </div>
       </div>
     </div>
   );
